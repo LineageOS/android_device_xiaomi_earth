1165626621e69524b07731671413e4ca28620a82
Giovanni Ricca <giovanniricca@proton.me>
2025-01-08 12:55:07 +0100

    earth: `libshim_sink.so` -> `libaudioclient_shim.so`
    Change-Id: I27c7cea3f2b7feda9736b9f1b1aa52db9202736e

--- extract-files.py
+++ extract-files.py
@@ -40,7 +40,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService.patch', '-r'),
     'system_ext/lib64/libsink.so': blob_fixup()
-        .add_needed('libshim_sink.so'),
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()

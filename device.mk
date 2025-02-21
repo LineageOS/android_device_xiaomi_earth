37e46b732a2aeac524250471b77a7e3ed0a38554
LuK1337 <priv.luk@gmail.com>
2025-02-21 09:01:04 +0100

    earth: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_COPY_FILES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # Media
 PRODUCT_COPY_FILES += \

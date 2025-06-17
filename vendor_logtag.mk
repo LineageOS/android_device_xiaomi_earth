27b8e0d1e34f5ac847572dd312d37197559ed0e8
ChrisCatto <chriscatto@proton.me>
2025-06-17 20:00:33 +0530

    earth: Silence HWUI logspam
    * After Android 15 QPR2, HWUI keeps spamming Image decoding logging
      dropped! so mute it since any other information from it is not
      really any more useful.
    
    Change-Id: Ifec5935cd92e2c3c242da4f791126fe94f3da451
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -114,6 +114,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.C2K_RIL-DATA=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.GsmCdmaPhone=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.RILMD2-SS=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.CapaSwitch=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.DSSelector=$(VENDOR_LOG_LEVEL) \

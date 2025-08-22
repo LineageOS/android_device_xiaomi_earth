5ce9d42e0b2be08e6ed6594db33eb3c47cf595af
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-22 16:28:48 +0800

    earth: libinit: Workaround crashes caused by build fp override on 16
    * Requires Id563c8aaa52c0e41823dbb035f0319a6531f12b7
    
    Change-Id: Ieb1fec7d51f0f8974c9baa4ebf50b8041ffceaa4

--- libinit/init_xiaomi_earth.cpp
+++ libinit/init_xiaomi_earth.cpp
@@ -61,8 +61,14 @@ static const std::vector<variant_info_t> variants = {
     earth_p_info,
 };
 
-void vendor_load_properties() {
+void vendor_process_bootenv() {
     if (access("/system/bin/recovery", F_OK) != 0) {
         search_variant(variants);
     }
+}
+
+void vendor_load_properties() {
+#if __ANDROID_API__ < 36
+    vendor_process_bootenv();
+#endif
 }

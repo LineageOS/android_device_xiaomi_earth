baa9cd5d984e96c668b4a1b9ad90763a4fa0953a
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-22 16:35:17 +0800

    [?] earth: Switch to OSS USB HALs
    Change-Id: I64a7186d437dcca2461c1c7761901a92c7ea0300

--- device.mk
+++ device.mk
@@ -269,6 +269,11 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
+
 # Vibrator
 $(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
 

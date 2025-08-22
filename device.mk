5209e8a7c49506da534c8ece42251e554e65c25b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-23 00:51:45 +0800

    earth: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- device.mk
+++ device.mk
@@ -164,6 +164,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayEarth \
+    DeviceAsWebcamOverlayEarth \
     FrameworksResOverlayEarth \
     SystemUIOverlayEarth \
     TelephonyOverlayEarth \

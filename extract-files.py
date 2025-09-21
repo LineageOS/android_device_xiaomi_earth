426a945376943644a41fcc629bbde0b3f0cc7336
Sarthak Roy <sarthakroy2002@gmail.com>
2025-09-21 09:46:54 +0200

    earth: Update VT from rothko A16
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I0b3dd9dc015d5eaa36847890fe6adc3be17374ab

--- extract-files.py
+++ extract-files.py
@@ -38,10 +38,6 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService.patch', '-r'),
-    'system_ext/lib64/libsink-mtk.so': blob_fixup()
-        .add_needed('libaudioclient_shim.so'),
-    'system_ext/lib64/libsource.so': blob_fixup()
-        .add_needed('libui_shim.so'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
